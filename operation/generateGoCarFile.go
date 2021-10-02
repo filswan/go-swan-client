9e7a68f5485bed8e91639de6f4a7f38dd83ece62
DoraNebula <fchen@nbai.io>
2021-10-02 15:25:23 -0600

    go car file

--- operation/generateGoCarFile.go
+++ operation/generateGoCarFile.go
@@ -4,22 +4,24 @@ import (
 	"go-swan-client/common/utils"
 	"go-swan-client/config"
 	"go-swan-client/logs"
+	"io/ioutil"
+	"strconv"
 
 	"github.com/google/uuid"
 )
 
-func GenerateGoCarFiles(inputDir, outputDir *string) {
-	generateMd5:=config.GetConfig().Sender.GenerateMd5
+func GenerateGoCarFiles(inputDir, outputDir *string) bool {
+	generateMd5 := config.GetConfig().Sender.GenerateMd5
 
-	if outputDir==nil{
-		outDir:=utils.GetDir(config.GetConfig().Sender.OutputDir, uuid.NewString())
-		outputDir=&outDir
+	if outputDir == nil {
+		outDir := utils.GetDir(config.GetConfig().Sender.OutputDir, uuid.NewString())
+		outputDir = &outDir
 	}
 
-	err:=utils.CreateDir(*outputDir)
-	if err!=nil{
+	err := utils.CreateDir(*outputDir)
+	if err != nil {
 		logs.GetLogger().Error(err)
-		return
+		return false
 	}
 
 	carFiles := []*FileDesc{}
@@ -27,42 +29,37 @@ func GenerateGoCarFiles(inputDir, outputDir *string) {
 	srcFiles, err := ioutil.ReadDir(*inputDir)
 	if err != nil {
 		logs.GetLogger().Error(err)
-		return
+		return false
 	}
 
-	for _, srcFile :=range srcFiles{
+	for _, srcFile := range srcFiles {
 		carFile := FileDesc{}
-		carFile.SourceFileName=srcFile.Name()
+		carFile.SourceFileName = srcFile.Name()
 		carFile.SourceFilePath = utils.GetDir(*inputDir, carFile.SourceFileName)
 		carFile.SourceFileSize = strconv.FormatInt(utils.GetFileSize(carFile.SourceFilePath), 10)
-		carFile.CarFileMd5=generateMd5
+		carFile.CarFileMd5 = generateMd5
 
 		carFiles = append(carFiles, &carFile)
 	}
+
+	result := GenerateGoCar(carFiles, *outputDir)
+
+	return result
 }
 
-func GenerateGoCar(carFiles []*FileDesc, outputDir string){
-	for _, carFile:=range carFiles{
-		carFile.CarFileName=carFile.SourceFileName+".car"
-		carFile.CarFilePath=utils.GetDir(outputDir,carFile.CarFileName)
+func GenerateGoCar(carFiles []*FileDesc, outputDir string) bool {
+	for _, carFile := range carFiles {
+		carFile.CarFileName = carFile.SourceFileName + ".car"
+		carFile.CarFilePath = utils.GetDir(outputDir, carFile.CarFileName)
 
+		dataCid, carFileName, isSucceed := utils.GraphSlit(outputDir, carFile.SourceFileName, carFile.CarFilePath)
+		if !isSucceed {
+			logs.GetLogger().Error("Failed to generate car file.")
+			return false
+		}
+		carFile.DataCid = *dataCid
+		carFile.CarFileName = *carFileName
 	}
+
+	return true
 }
-for _deal in _deal_list:
-    command_line = "./graphsplit chunk --car-dir={} --slice-size=1000000000 --parallel=2 --graph-name={} --calc-commp=true --parent-path=. {}".format(target_dir, _deal.source_file_name,  _deal.source_file_path)
-    subprocess.run((command_line), shell=True)
-    
-    with open(os.path.join(target_dir,"manifest.csv"),newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-             if row["filename"] == car_file_name  : 
-                 datacid = row["playload_cid"] 
-                 car_file_path = os.path.join(target_dir, row["playload_cid"] +'.car')
-                 piececid = row["piece_cid"]
-                 car_file_name = row["playload_cid"] +'.car'
-                 break
-     
-    # no piece_cid generated. use data_cid instead
-    data_cid=datacid
-    piece_cid = piececid
-           
