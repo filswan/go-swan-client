741f4a8d8c7c79c74f1eacccd6cc5521a4b48862
DoraNebula <fchen@nbai.io>
2021-10-26 08:58:52 -0600

    Update build_from_source.sh

--- build_from_source.sh
+++ build_from_source.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+CONF_FILE_PATH=${HOME}/.swan/client/config.toml
+echo $CONF_FILE_PATH
+
+if [ -f "${CONF_FILE_PATH}" ]; then
+    echo "~/.swan/client/config.toml exists"
+else
+    cp ./config/config.toml.example $CONF_FILE_PATH
+    echo "~/.swan/client/config.toml created"
+fi
+
 git submodule update --init --recursive
 make ffi
 make
 
-if [ -f "~/.swan/client/config.toml" ]; then
-    echo "~/.swan/client/config.toml exists"
-else
-    cp ./config/config.toml.example ~/.swan/client/config.toml
-    echo "~/.swan/client/config.toml created"
-fi
