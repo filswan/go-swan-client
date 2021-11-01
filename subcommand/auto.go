c7a4bfd1446806af6397407cf96579841307b31a
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2021-11-01 11:25:47 -0600

    send deals for one task

--- subcommand/auto.go
+++ subcommand/auto.go
@@ -181,13 +181,12 @@ func SendAutoBidDealByTaskUuid(confDeal *model.ConfDeal, taskUuid string) (int, 
 	task := assignedTaskInfo.Data.Task
 	dealSentNum, csvFilePath, carFiles, err := SendAutobidDeals4Task(confDeal, deals, task, confDeal.OutputDir)
 	if err != nil {
-		csvFilepaths = append(csvFilepaths, csvFilePath)
 		logs.GetLogger().Error(err)
 		return 0, "", nil, err
 	}
 
 	if dealSentNum == 0 {
-		err := fmt.Errorf(dealSentNum, " deal(s) sent for task:", task.TaskName)
+		err := fmt.Errorf("%d deal(s) sent for task:%s", dealSentNum, task.TaskName)
 		logs.GetLogger().Info(err)
 		return 0, "", nil, err
 	}
@@ -197,7 +196,7 @@ func SendAutoBidDealByTaskUuid(confDeal *model.ConfDeal, taskUuid string) (int, 
 		status = constants.TASK_STATUS_PROGRESS_WITH_FAILURE
 	}
 
-	response, err := swanClient.SwanUpdateAssignedTask(*assignedTask.Uuid, status, csvFilePath)
+	response, err := swanClient.SwanUpdateAssignedTask(taskUuid, status, csvFilePath)
 	if err != nil {
 		logs.GetLogger().Error(err)
 		return 0, "", nil, err
