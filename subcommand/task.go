b4c2558488c234091a4e5da7bcc54216c30b7477
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2021-11-04 11:43:45 -0600

    Update task.go

--- subcommand/task.go
+++ subcommand/task.go
@@ -157,6 +157,7 @@ func SendTask2Swan(confTask *model.ConfTask, task libmodel.Task, carFiles []*lib
 	}
 
 	logs.GetLogger().Info("task duration:", task.Duration)
+	logs.GetLogger().Info("jwt token:", swanClient.JwtToken)
 	swanCreateTaskResponse, err := swanClient.SwanCreateTask(task, csvFilePath)
 	if err != nil {
 		logs.GetLogger().Error(err)
