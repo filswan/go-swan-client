e7e4f6dcb91e9a3c40ed90261482c9929b2de7ad
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2021-12-31 14:17:50 -0600

    main

--- test/test.go
+++ test/test.go
@@ -16,8 +16,8 @@ func Test() {
 	//TestCreateTask()
 	//TestCreateTasks(1)
 	//TestSendDeals()
-	//TestSendAutoBidDeals()
-	TestSendAutoBidDealsByTaskUuid()
+	TestSendAutoBidDeals()
+	//TestSendAutoBidDealsByTaskUuid()
 }
 
 func TestCreateCarFiles() {
@@ -98,7 +98,7 @@ func TestSendDeals() {
 
 	outDir := filepath.Join(homeDir, "work/carFiles")
 
-	command.SendDealsByConfig(outDir, "t03354", "/Users/dorachen/work/carFiles/swan-task-q79yx3-metadata.json")
+	command.SendDealsByConfig(outDir, "t03354", "/Users/dorachen/work/carFiles/swan-task-oe1p20-metadata.json")
 }
 
 func TestSendAutoBidDeals() {
