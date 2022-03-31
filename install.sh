ad22cd3ef846161b477f5b56f370846ce3d49a02
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-03-31 08:34:45 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+CODE_URL=https://github.com/filswan/go-swan-client
+BINARY_NAME=swan-client
+TAG_NAME=v0.1.0
+
+wget ${CODE_URL}/releases/download/${TAG_NAME}/${BINARY_NAME}
+wget ${CODE_URL}/releases/download/${TAG_NAME}/config.toml
+
 CONF_FILE_DIR=${HOME}/.swan/client
 mkdir -p ${CONF_FILE_DIR}
 
