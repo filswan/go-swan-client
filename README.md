70668f842aa60d704014bbb28669f1ae11c6d88d
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-04-01 14:39:53 -0600

    --no-check-certificate

--- README.md
+++ README.md
@@ -84,7 +84,7 @@ In swan project, a task can contain one or multiple offline deal(s).
 ## Installation
 ### Option:one:  **Prebuilt package**: See [release assets](https://github.com/filswan/go-swan-client/releases)
 ```shell
-wget https://github.com/filswan/go-swan-client/releases/download/v0.1.0-rc1/install.sh
+wget https://github.com/filswan/go-swan-client/releases/download/v0.1.0-rc1/install.sh --no-check-certificate
 chmod +x ./install.sh
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@ CODE_URL=https://github.com/filswan/go-swan-client
 BINARY_NAME=swan-client
 TAG_NAME=v0.1.0-rc1
 
-wget ${CODE_URL}/releases/download/${TAG_NAME}/${BINARY_NAME}
-wget ${CODE_URL}/releases/download/${TAG_NAME}/config.toml.example
+wget ${CODE_URL}/releases/download/${TAG_NAME}/${BINARY_NAME} --no-check-certificate
+wget ${CODE_URL}/releases/download/${TAG_NAME}/config.toml.example --no-check-certificate
+
+chmod +x ./${BINARY_NAME}
 
 CONF_FILE_DIR=${HOME}/.swan/client
 mkdir -p ${CONF_FILE_DIR}
 
 CONF_FILE_PATH=${CONF_FILE_DIR}/config.toml
-echo $CONF_FILE_PATH
+echo "config file path is: $CONF_FILE_PATH"
 
 if [ -f "${CONF_FILE_PATH}" ]; then
     echo "${CONF_FILE_PATH} exists"
